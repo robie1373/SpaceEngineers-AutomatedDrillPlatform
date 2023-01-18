eff174592dbe2683e584908487cf2b903770da15
Robie <robie1373@gmail.com>
2023-01-18 19:09:36 +0000

    drill onoff velocity and maxposition groundwork for statemachine

--- Automated-Drill-Platform/_Main_v0.0.1.cs
+++ Automated-Drill-Platform/_Main_v0.0.1.cs
@@ -6,8 +6,16 @@
 
 IMyBlockGroup ygroup;
 IMyBlockGroup zgroup;
-List<IMyTerminalBlock> yblocks = new List<IMyTerminalBlock>();
-List<IMyTerminalBlock> zblocks = new List<IMyTerminalBlock>();
+List<IMyExtendedPistonBase> ypistons = new List<IMyExtendedPistonBase>();
+List<IMyExtendedPistonBase> zpistons = new List<IMyExtendedPistonBase>();
+List<IMyShipDrill> drills = new List<IMyShipDrill>();
+public string platform_status; // Use for state machine controlling platform
+// boring -> y extending drills on
+// lifting -> y retracting drills on
+// extending -> z extending drills on
+// rotating -> rotor moving drills on
+// terminating -> z retracting drills off
+
 
 //// Program()
 // for variable initialization, setup, etc.
@@ -19,7 +27,15 @@ public Program() {
 public void Initialize() {
   // run each Program__...() submethods here
   Program__GetPistons();
-
+  //List<IMyShipDrill> drills = new List<IMyShipDrill>();
+  GridTerminalSystem.GetBlocksOfType(drills);
+  foreach (var drill in drills)
+  {
+    drill.ApplyAction("OnOff_On");
+  }
+  set_velocity(ypistons, yExtensionVelocity);
+  set_velocity(zpistons, zExtensionVelocity);
+  platform_status = "boring";
 } // Initialize()
 
 public void Save() {
@@ -28,21 +44,52 @@ public void Save() {
 public void Main(string argument, UpdateType updateSource) {
   // NOTE: multiple trigger sources can roll in on the same tick
   // test each trigger individually, not with if() else if () blocks
-
-  if((updateSource & UpdateType.Update100) != 0) { // TODO: can != 0 be dropped? // had to delete source check due to compile error
+  if((updateSource & UpdateType.Update100) != 0) { // TODO: can != 0 be dropped? 
     // run each Main__...() submethod here
-  //Main__WriteDiagnostics(); // This method was in _Template
+    set_max_limit(zpistons, 2.5F);
+    extend_piston(zpistons);
+    if (is_at_full_extension(ypistons)) {
+      Echo("hole is deep");
+      set_velocity(ypistons, -0.5F);
+      all_stop();
+    } else {
+      Echo("hole not deep");
+    }
+        get_status(ypistons);
+  }
+  switch (platform_status) {
+    case "boring":
+      //do things
+      break;
+    case "lifting":
+      // do things
+      break;
+    case "extending":
+      //do things
+      break;
+    case "rotating":
+      //do things
+      break;
+    case "terminating":
+      //do things
+      break;
+    default:
+      //do things
+      break;
   }
 } // Main()
 
 public void Program__GetPistons() {
+
   ygroup = GridTerminalSystem.GetBlockGroupWithName(yPistonGroup);
   if (ygroup == null)
   {
     Echo("Y Group not found");
     return;
   }
-  get_status(ygroup);
+  //var ypistons = make_list_from_group(ygroup, ypistons);
+  make_list_from_group(ygroup, ypistons);
+  get_status(ypistons);
 
   zgroup = GridTerminalSystem.GetBlockGroupWithName(zPistonGroup);
   if (zgroup == null)
@@ -50,19 +97,67 @@ public void Program__GetPistons() {
     Echo("Z Group not found");
     return;
   }
-  get_status(zgroup);
-
+  //var zpistons = make_list_from_group(zgroup, zpistons);
+  make_list_from_group(zgroup, zpistons);
+  get_status(zpistons);
 }
 
-public void get_status(IMyBlockGroup group) {
-  Echo($"{group.Name}:");
-  List<IMyExtendedPistonBase> pistons = new List<IMyExtendedPistonBase>();
+public void make_list_from_group(IMyBlockGroup group, List<IMyExtendedPistonBase> pistons) {
+  //List<IMyExtendedPistonBase> pistons = new List<IMyExtendedPistonBase>();
   group.GetBlocksOfType(pistons);
+  //return pistons;
+}
+
+public void get_status(List<IMyExtendedPistonBase> pistons) {
+  //Echo($"{group.Name}:");
+  //var pistons = make_list_from_group(group);
   foreach (var piston in pistons)
   {
     Echo($"- {piston.CustomName}");
     Echo($"position: {piston.CurrentPosition}");
     Echo($"MaxLimit: {piston.MaxLimit}");
+    Echo($"Velocity: {piston.Velocity}");
+  }
+}
 
+public void set_max_limit(List<IMyExtendedPistonBase> pistons, float new_limit) {
+ foreach (var piston in pistons)
+  {
+    //Echo($"Old MaxLimit: {piston.MaxLimit}");
+    piston.MaxLimit = new_limit;
+    //Echo($"New MaxLimit: {piston.MaxLimit}");
+  }
+}
+
+public void set_velocity(List<IMyExtendedPistonBase> pistons, float new_velocity) {
+  foreach (var piston in pistons)
+  {
+    piston.Velocity = new_velocity;
+  }
+}
+
+public void extend_piston(List<IMyExtendedPistonBase> pistons) {
+  foreach (var piston in pistons)
+  {
+    piston.Extend();
+  }
+}
+
+public void all_stop() {
+ foreach (var drill in drills)
+  {
+    drill.ApplyAction("OnOff_Off");
+    Echo("All drills stopped");
+    platform_status = "terminating";
+  }
+}
+
+public bool is_at_full_extension(List<IMyExtendedPistonBase> pistons)
+{
+  if (pistons[0].CurrentPosition == 10) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
